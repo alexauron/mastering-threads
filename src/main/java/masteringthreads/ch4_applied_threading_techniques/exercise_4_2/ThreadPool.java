2beea596f5052c4bfe0c3229952a5d0685667adb
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2023-01-27 19:21:47 +0200

    exercise 4.2 done and 4.4 half done

--- src/main/java/masteringthreads/ch4_applied_threading_techniques/exercise_4_2/ThreadPool.java
+++ src/main/java/masteringthreads/ch4_applied_threading_techniques/exercise_4_2/ThreadPool.java
@@ -1,55 +1,27 @@
 package masteringthreads.ch4_applied_threading_techniques.exercise_4_2;
 
-import java.util.*;
 import java.util.concurrent.*;
 
 // TODO: Replace inner workings of ThreadPool with ExecutorService
 public class ThreadPool {
-    private final BlockingQueue<Runnable> tasks = new LinkedBlockingQueue<>();
-    private final Collection<Worker> workers = new ConcurrentLinkedQueue<>();
+    private final ThreadPoolExecutor pool;
     private volatile boolean running = true;
 
     public ThreadPool(int poolSize) {
-        for (int i = 0; i < poolSize; i++) {
-            var worker = new Worker("worker-" + i);
-            worker.start();
-            workers.add(worker);
-        }
-    }
-
-    private Runnable take() throws InterruptedException {
-        return tasks.take();
+        pool = new ThreadPoolExecutor(poolSize, poolSize,
+                0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>());
     }
 
     public void submit(Runnable job) {
-        tasks.add(job);
+        pool.submit(job);
     }
 
     public int getRunQueueLength() {
-        return tasks.size();
+        return pool.getQueue().size();
     }
 
     public void shutdown() {
-        running = false;
-        workers.forEach(Thread::interrupt);
-    }
-
-    private class Worker extends Thread {
-        public Worker(String name) {
-            super(name);
-        }
-
-        public void run() {
-            // we run in an infinite loop:
-            while (running) {
-                // remove the next task from the linked list using take()
-                // we then call the run() method on the job
-                try {
-                    take().run();
-                } catch (InterruptedException consumeAndExit) {
-                    break;
-                }
-            }
-        }
+        pool.shutdownNow();
     }
 }
--- src/main/java/masteringthreads/ch4_applied_threading_techniques/exercise_4_4/HouseDrawing.java
+++ src/main/java/masteringthreads/ch4_applied_threading_techniques/exercise_4_4/HouseDrawing.java
@@ -1,14 +1,22 @@
 package masteringthreads.ch4_applied_threading_techniques.exercise_4_4;
 
+import java.util.*;
+
 public class HouseDrawing extends StupidFramework {
     private final String colour;
-
+    private static String tempColour;
+    private static String saveColour(String title, String colour) {
+        tempColour = colour;
+        return title;
+    }
     public HouseDrawing(String title, String colour) {
-        super(title);
+        super(saveColour(title, colour));
         this.colour = colour;
     }
-
+    private String getColour() {
+        return Objects.requireNonNullElse(colour, tempColour);
+    }
     public void draw() {
-        System.out.println("Drawing house with colour " + colour);
+        System.out.println("Drawing house with colour " + getColour());
     }
 }
